25f68b36eb6f0d8f211cfe642de57bc11df4e449
bontang828 <bontang828@gmail.com>
2024-06-17 11:20:09 +0100

    MORE ON MODE_SELECTOR.V

--- Testbenches/Updated project/V4.1 - adding Top with Line Buffer Timing/mode_selector.v
+++ Testbenches/Updated project/V4.1 - adding Top with Line Buffer Timing/mode_selector.v
@@ -12,6 +12,8 @@ module mode_selector #(
     input   [X_SIZE-1:0]    parallel_next_state_result,
     input   [Y_SIZE-1:0]    parallel_next_state_write_addr,
     input                   parallel_next_state_write_en,
+    input                   y,
+    output  [X_SIZE-1:0]    topline_out,
 
     //ram A
     output  [Y_WIDTH-1:0]   BRAM_A_addra,
@@ -33,7 +35,36 @@ module mode_selector #(
     input   [X_SIZE-1:0]    BRAM_B_doutb,
     output                  BRAM_B_web
 );
-    
+
+//all READ port 'a' for line buffer
+//all READ port 'b' for outstream video
+
+//mode 0: RAM_A read, RAM_B write
+
+always @(posedge) //use always comb
+    if(mode) begin
+        BRAM_A_addra <= line_buffer_fetch_addr;
+        BRAM_A_addrb <= y;
+        line_buffer_fetch_mem <= BRAM_A_douta;
+        topline_out <= BRAM_A_doutb;
+
+        BRAM_B_addra <= parallel_next_state_write_addr;
+        BRAM_B_dina <= parallel_next_state_result;
+        BRAM_B_wea <= parallel_next_state_write_en;
+    end else begin
+        BRAM_B_addra <= line_buffer_fetch_addr;
+        BRAM_B_addrb <= y;
+        line_buffer_fetch_mem <= BRAM_B_douta;
+        topline_out <= BRAM_B_doutb;
+
+        BRAM_A_addra <= parallel_next_state_write_addr;
+        BRAM_A_dina <= parallel_next_state_result;
+        BRAM_A_wea <= parallel_next_state_write_en;
+    end
+
+
+
+end
 
 
 endmodule
