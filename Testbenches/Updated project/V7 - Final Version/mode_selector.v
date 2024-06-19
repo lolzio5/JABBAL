e5daa810ceb3fddb06c4bb908f300fa0f169a251
lolzio5 <loleziovioramarquet@gmail.com>
2024-06-20 00:36:15 +0100

    combined logic together

---
+++ Testbenches/Updated project/V7 - Final Version/mode_selector.v
@@ -0,0 +1,98 @@
+module mode_selector #(
+    parameter X_SIZE = 1280,
+    parameter Y_SIZE =720,
+    parameter X_WIDTH = 11,
+    parameter Y_WIDTH = 10
+) (
+    input                   clk,
+    input                   mode, //PAUSE FLAG
+    
+    input   [Y_WIDTH-1:0]   line_buffer_fetch_addr,
+    output  [X_SIZE-1:0]    line_buffer_fetch_mem,
+    input   [Y_WIDTH-1:0]    parallel_next_state_write_addr,
+    input   [X_SIZE-1:0]    parallel_next_state_result,
+    input                   parallel_next_state_write_en,
+    input   [Y_WIDTH-1:0]   video_out_row_addr,
+    output  [X_SIZE-1:0]    video_out_row_data,
+
+    //ram A
+    output  [Y_WIDTH-1:0]   BRAM_A_addra,
+    output  [X_SIZE-1:0]   BRAM_A_dina,
+    input   [X_SIZE-1:0]    BRAM_A_douta,
+    output                  BRAM_A_wea,
+    output  [Y_WIDTH-1:0]   BRAM_A_addrb,
+    output  [X_SIZE-1:0]    BRAM_A_dinb,
+    input   [X_SIZE-1:0]    BRAM_A_doutb,
+    output                  BRAM_A_web,
+
+    //ram B
+    output  [Y_WIDTH-1:0]   BRAM_B_addra,
+    output  [X_SIZE-1:0]   BRAM_B_dina,
+    input   [X_SIZE-1:0]    BRAM_B_douta,
+    output                  BRAM_B_wea,
+    output  [Y_WIDTH-1:0]   BRAM_B_addrb,
+    output  [X_SIZE-1:0]    BRAM_B_dinb,
+    input   [X_SIZE-1:0]    BRAM_B_doutb,
+    output                  BRAM_B_web
+);
+
+//all READ port 'a' for line buffer
+//all READ port 'b' for outstream video
+
+
+// reg [Y_WIDTH-1:0]   BRAM_A_addra_reg;
+// reg [Y_WIDTH-1:0]   BRAM_A_addrb_reg;
+// reg [X_SIZE-1:0]    line_buffer_fetch_mem_reg;
+// reg [X_SIZE-1:0]    video_out_row_data_reg;
+
+// reg [Y_WIDTH-1:0]   BRAM_B_addra_reg;
+// reg [X_SIZE-1:0]    BRAM_B_dina_reg;
+// reg                 BRAM_B_wea_reg;
+
+// reg [Y_WIDTH-1:0]   BRAM_B_addrb_reg;
+// reg [X_SIZE-1:0]    BRAM_A_dina_reg;
+// reg                 BRAM_A_wea_reg;
+
+//mode 0: RAM_A read, RAM_B write
+
+assign  BRAM_A_addra = mode ? line_buffer_fetch_addr : parallel_next_state_write_addr;
+assign  BRAM_A_addrb = video_out_row_addr;
+assign  BRAM_B_addrb = video_out_row_addr;
+assign  line_buffer_fetch_mem = mode ? BRAM_A_douta : BRAM_B_douta;
+assign  video_out_row_data = mode ? BRAM_A_doutb : BRAM_B_doutb;
+assign  BRAM_B_addra = mode ? parallel_next_state_write_addr : line_buffer_fetch_addr;
+assign  BRAM_B_dina = parallel_next_state_result;
+assign  BRAM_A_dina = parallel_next_state_result;
+assign  BRAM_B_wea = mode ? 1 : 0;   // assign  BRAM_B_wea = mode ? parallel_next_state_write_en : 0;
+assign  BRAM_A_wea = mode ? 0 : 1;   // assign  BRAM_A_wea = mode ? 0 : parallel_next_state_write_en;
+assign  BRAM_A_web = 1'b0;
+assign  BRAM_B_web = 1'b0;
+assign BRAM_A_dinb = 1280'b0;
+assign  BRAM_B_dinb  = 1280'b0;
+
+
+
+
+
+// always @(*) begin //use always comb
+//     if(mode) begin
+//         BRAM_A_addra = line_buffer_fetch_addr;
+//         BRAM_A_addrb = video_out_row_addr;
+//         line_buffer_fetch_mem = BRAM_A_douta;
+//         video_out_row_data = BRAM_A_doutb;
+
+//         BRAM_B_addra = parallel_next_state_write_addr;
+//         BRAM_B_dina = parallel_next_state_result;
+//         BRAM_B_wea = parallel_next_state_write_en;
+//     end else begin
+//         BRAM_B_addra = line_buffer_fetch_addr;
+//         BRAM_B_addrb = video_out_row_addr;
+//         line_buffer_fetch_mem = BRAM_B_douta;
+//         video_out_row_data = BRAM_B_doutb;
+
+//         BRAM_A_addra = parallel_next_state_write_addr;
+//         BRAM_A_dina = parallel_next_state_result;
+//         BRAM_A_wea = parallel_next_state_write_en;
+//     end
+//end
+endmodule
---
+++ Testbenches/Updated project/V7 - Final Version/parallel_next_state.v
@@ -0,0 +1,55 @@
+module parallel_next_state #(
+    parameter row_length = 1280
+) (
+    input wire clk,
+    input wire [row_length-1:0] top_row,    // FROM line_buffer
+    input wire [row_length-1:0] middle_row, // FROM line_buffer
+    input wire [row_length-1:0] bottom_row, // FROM line_buffer
+    output wire [row_length-1:0] result, // TO BRAM write
+
+    // New
+    input [9:0] calc_row_in,    // FROM line_buffer
+    input calc_flg,             // FROM line_buffer
+    input valid_set,            // FROM line_buffer
+    output [9:0] write_addr,    // To BRAM write
+    output write_en             // TO BRAM write
+);
+
+    wire [row_length+1:0] padded_top_row = {1'b0, top_row, 1'b0};
+    wire [row_length+1:0] padded_middle_row = {1'b0, middle_row, 1'b0};
+    wire [row_length+1:0] padded_bottom_row = {1'b0, bottom_row, 1'b0};
+    
+    // Generate and wire together 1280 Conway single modules to calculate the next state for all at once
+    genvar i;
+    generate
+        for (i = 0; i < row_length ; i = i + 1) begin : accelerator
+            single_next_state single_next_state(
+                .top_row(padded_top_row[i+2:i]),
+                .middle_row(padded_middle_row[i+2:i]),
+                .bottom_row(padded_bottom_row[i+2:i]),
+                .next_state(result[i])
+            );
+        end
+    endgenerate
+
+    // NEW STUFF
+
+    reg [9:0] write_addr_reg;
+    assign write_addr = write_addr_reg;
+
+    reg write_en_reg;
+    assign write_en = write_en_reg;
+
+    always @(posedge clk) begin
+        write_addr_reg <= calc_row_in;   // Sends address to write to
+        
+        // SET WRITE ENABLE LOW IF NOT VALID OR IF FINISHED WIRTING - prevents unwanted/corrupt writing 
+        if (valid_set && calc_flg) begin // Only writes if valid and we set calc_flg high
+            write_en_reg <= 1;
+        end
+        else begin
+            write_en_reg <= 0;
+        end
+    end
+    
+endmodule
